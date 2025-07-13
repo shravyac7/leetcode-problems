d7618c28da2397e87be4e6eb69e5c8edc51c1c7b
BEEM REDDY SHRAVYA REDDY <beemreddyshravyareddy44@gmail.com>
2025-07-13 15:19:33 +0530

    Add files via upload

---
+++ combination sum 2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def generate(self,ind,curr,ans,candidates,target):
+        if(target==0):
+            ans.append(curr.copy())
+            return
+        if(target<0 or ind==len(candidates)):
+            return
+        curr.append(candidates[ind])
+        self.generate(ind+1,curr,ans,candidates,target-candidates[ind])
+        curr.pop()
+        for i in range(ind+1,len(candidates)):
+            if(candidates[ind]!=candidates[i]):
+                self.generate(i,curr,ans,candidates,target)
+                break
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        ind=0
+        curr=[]
+        ans=[]
+        self.generate(ind,curr,ans,candidates,target)
+        return ans
+
+        
